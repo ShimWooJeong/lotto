87cdce11b4d2233bbff59f536b41a4fe6d6fab38
ShimWooJeong <dnwjd040635@gmail.com>
2021-05-27 15:15:12 +0900

    updated lotto ball image

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -38,6 +38,17 @@ fun getRandomLottoNumbers(): MutableList<Int>{
     return lottoNumbers
 }
 
+fun getShuffledLottoNumbers() : MutableList<Int>{
+    val list = mutableListOf<Int>()
+
+    for(number in 1..45){
+        list.add(number)
+    }
+    list.shuffle()
+
+    return list.subList(0, 6);
+}
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +58,7 @@ class MainActivity : AppCompatActivity() {
         cv_random.setOnClickListener {
             val intent = Intent(this, ResultActivity::class.java)
             intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+            intent.putIntegerArrayListExtra("result", ArrayList(getShuffledLottoNumbers()))
             startActivity(intent)
         }
 
