2cdc89d88e917585388bf0a7d4c472549f87f7e7
ShimWooJeong <dnwjd040635@gmail.com>
2021-05-27 13:25:36 +0900

    Random number generation

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -4,6 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.cardview.widget.CardView
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.random.Random.Default.nextInt
+
+fun getRandomLottoNumber (): Int{
+    return Random().nextInt(45)+1
+}
+
+fun getRandomLottoNumbers(): MutableList<Int>{
+    val lottoNumbers = mutableListOf<Int>()
+
+    while(true) {
+        val number = getRandomLottoNumber()
+        var flag_exist = 0
+
+        if (lottoNumbers.size < 1) {
+            lottoNumbers.add(number)
+            continue
+        } else {
+            for (j in 0 until lottoNumbers.size) {
+                if (number == lottoNumbers[j]) {
+                    flag_exist = 1
+                    break
+                }
+            }
+            if (flag_exist == 0)
+                lottoNumbers.add(number)
+            if (lottoNumbers.size >= 6)
+                break
+        }
+    }
+    return lottoNumbers
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +45,9 @@ class MainActivity : AppCompatActivity() {
 
         val cv_random = findViewById<CardView>(R.id.cv_random)
         cv_random.setOnClickListener {
-            startActivity(Intent(this@MainActivity, ResultActivity::class.java))
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+            startActivity(intent)
         }
 
         val cv_constell = findViewById<CardView>(R.id.cv_constell)
